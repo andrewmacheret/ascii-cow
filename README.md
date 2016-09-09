beb27f842d49909a9cf098590fadd0db2cdc2dd7
Andrew Macheret <andrew.macheret@gmail.com>
2016-09-08 23:44:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ascii-cow
+
+Give it an image and a size, and get colorful ascii art spoken by a cow.
+
+Dependencies:
+
+  * [im2a](https://github.com/tzvetkoff/im2a)
+  * [cowsay](https://en.wikipedia.org/wiki/Cowsay)
+  * [coreutils](https://www.gnu.org/software/coreutils/coreutils.html) - for gsed
+
+Example usage:
+
+  ```bash
+  ./ascii-cow.sh example.jpg 80 40
+  ```
+
+  ![Screenshot](screenshot.png?raw=true "Screenshot")
---
+++ ascii-cow.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+usage() {
+  echo "$( basename "$0" ) <IMAGE-FILE> <WIDTH> <HEIGHT>" 1>&2
+  exit 1
+}
+
+[[ "$#" -eq 3 ]] || usage
+
+IMAGE_FILE="$1"
+WIDTH="$2"
+HEIGHT="$3"
+
+which im2a >/dev/null || {
+  echo "im2a required. Run `brew install tzvetkoff/extras/im2a` on OS/X or visit https://github.com/tzvetkoff/im2a for alternatives." 1>&2
+  exit 1
+}
+
+which im2a >/dev/null || {
+  echo "cowsay required. Run `brew install cowsay` on OS/X or check your package manager." 1>&2
+  exit 1
+}
+
+which gsed >/dev/null || {
+  echo "gsed required. Run `brew install coreutils` on OS/X or check your package manager." 1>&2
+  exit 1
+}
+
+im2a -W "$WIDTH" -H "$HEIGHT" "$IMAGE_FILE" | cowsay -e .o -n | gsed -r 's/(([-_]){'"$WIDTH"'})[-_]+/\1\2\2/' | gsed -r 's/ +([|\/\]) *$/ \1/'
